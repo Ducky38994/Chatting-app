df1ba8a283adaf46046852d6f2a91bb2bb02de8d
dal-chaas <gamerbrutalcup@gmail.com>
2026-01-24 16:55:31 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,22 +2,18 @@ importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyBK4pJpKtFPs19-J051xR6t77SBHYrg1k8",
-    authDomain: "subpls-2c774.firebaseapp.com",
-    projectId: "subpls-2c774",
-    messagingSenderId: "834082653588",
-    appId: "1:834082653588:web:a7b266363aa864249f3b81"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-    self.registration.showNotification(
-        payload.notification.title,
-        {
-            body: payload.notification.body,
-            icon: payload.notification.icon,
-            badge: payload.notification.icon
-        }
-    );
+    self.registration.showNotification(payload.notification.title, {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    });
 });
